67b990c2ce98a10b038905221e85f1a07dfe6cb7
dia-avv <eaa24avv@students.eaaa.dk>
2025-09-23 16:00:55 +0200

    My Work Page

--- src/pages/MyWork.tsx
+++ src/pages/MyWork.tsx
@@ -1,3 +1,45 @@
+import styles from "./MyWork.module.css";
+import { Link } from "react-router-dom";
+import projectsData from "../data/projects.json";
+
+type Project = {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  categories: string[];
+};
+
+const projects = projectsData as Project[];
+
 export default function MyWork() {
-    return <h1>My Work Page</h1>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>My Work</h1>
+      <div className={styles.projectList}>
+        {projects.map((project) => (
+          <Link
+            key={project.id}
+            to={`/projects/${project.id}`}
+            className={styles.card}
+          >
+            <div>
+              <img src={`${import.meta.env.BASE_URL}${project.image}`} alt={project.title} className={styles.coverPhoto}/>
+              <div>
+                <h2>{project.title}</h2>
+                <p>{project.description}</p>
+              </div>
+            </div>
+            <div className={styles.categories}>
+              {project.categories.map((category) => (
+                <span key={category} className={styles.category}>
+                  {category}
+                </span>
+              ))}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
